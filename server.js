1bc17d3cf76040067fa249e94cdfeb691f4793c9
abdulgamal <gamala@paysokosystems.com>
2025-08-12 21:15:07 +0300

    fix: added request demo endpoint

--- server.js
+++ server.js
@@ -68,6 +68,57 @@ app.post("/submit-form", (req, res) => {
   });
 });
 
+app.post("/request-demo", (req, res) => {
+  const { companyName, jobTitle, firstName, lastName, country, phoneNumber, inquiry } = req.body;
+
+  // Create a Nodemailer transporter
+  const transporter = nodemailer.createTransport({
+    service: "Gmail", // Change this to your email service provider
+    auth: {
+      user: "josephmaruria@gmail.com", // Change this to your email address
+      pass: "pkchatfzjxjqnuqk", // Change this to your email password
+    },
+  });
+
+  // Email message options
+  const mailOptions = {
+    from: "info@paysokosystems.com", // Change this to your website's email address
+    to: [
+      "josephmaruria@gmail.com",
+      "juliet.wamuyu@paysokosystems.com",
+      "info@paysokosystems.com",
+      "mosesk@paysokosystems.com",
+      "michaelc@paysokosystems.com",
+      "petem@paysokosystems.com",
+    ], // Change this to your desired email address
+    subject: "New Message from PaySoko Systems Request Demo Form",
+    text: `
+      Company Name: ${companyName}
+      Job Title: ${jobTitle}
+      First Name: ${firstName}
+      Last Name: ${lastName}
+      Country: ${country}
+      Phone Number: ${phoneNumber}
+      Inquiry: ${inquiry}
+    `,
+  };
+
+  // Send email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error sending email:", error);
+      res
+        .status(500)
+        .send("Oops! Something went wrong. Please try again later.");
+    } else {
+      console.log("Email sent:", info.response);
+      res.send(
+        "Thank you for contacting PaySoko Systems Inc. One of our agents will review your message and contact you shortly!"
+      );
+    }
+  });
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
